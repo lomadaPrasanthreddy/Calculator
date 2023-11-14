9010489fbefcd31d2e6b246823c7bdeca3723b69
vboxuser <vboxuser@ubuntuvm.myguest.virtualbox.org>
2023-11-14 15:18:18 +0530

    test 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,7 +25,6 @@ public class Main {
                 break;
             }
 
-
             System.out.print("Enter the first number: ");
             double num1 = scanner.nextDouble();
             System.out.print("Enter the second number: ");
